c5218c72a38590b80a55b673b76e56809e90d949
Dev Mohib <dev.mohib@gmail.com>
2023-12-07 15:59:28 +0500

    added projects page

---
+++ src/data/projects.ts
@@ -0,0 +1,74 @@
+export const projects = [
+    {
+        title : "Animatix",
+        image : "animatix.gif",
+        link : "https://animatix.web.app",
+        slug: "/animatix",
+        feature1: [
+            "HTML Canvas Drawing",
+            "Add multiple frames for drawig",
+            "Background and drawing color",
+            "Undo/Redo Function",
+        ],
+        feature2 : [
+            "Play frames",
+            "Manage frame speed",
+            "Record frames",
+            "Export as video"
+        ]
+    },
+    {
+        title : "LayerCraft",
+        image : "layercraft.gif",
+        link: "https://layercraft.web.app",
+        slug : "layercraft",
+        feature1 : [
+            "Record and play image layers",
+            "Apply animations on each layer",
+            "Order layers",
+            "Play multiple threads and frames"
+        ],
+        feature2 : [
+            "Play frames",
+            "Manage frame speed",
+            "Record frames",
+            "Export as video"
+        ]
+    },
+    {
+        title : "CodeYolo",
+        image: "codeyolo.gif",
+        link : "https://editor.codeyolo.com",
+        slug: "codeyolo",
+        feature1 :[
+            "Online Code Editor",
+            "Add HTML,CSS,Javascript Code",
+            "Add CSS,Javascript libraries",
+            "Preview the result"
+        ],
+        feature2 : [
+            "Save snippets to cloud",
+            "Post snippets for people",
+            "Snippet likes and comments",
+            "Fork other people snippets"
+        ]
+    },
+    {
+        title : "RewardWee",
+        image : "rewardwee.png",
+        link : "https://rewardwee.web.app",
+        slug : "rewardwee",
+        feature1 : [
+            "Wheel spin reward system",
+            "Signup as an organization",
+            "Add employees and rewards",
+            "Create a reward compaign",
+        ],
+        feature2: [
+            "Manage rewards values",
+            "Set rewards win probability",
+            "Invite employees to a compaign",
+            "Spin wheel to win a reward",
+        ]
+    }
+]
