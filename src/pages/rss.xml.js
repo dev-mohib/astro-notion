4c89d350dc0310946f4315040ed03a5e18aa0c81
Dev Mohib <dev.mohib@gmail.com>
2024-02-27 14:50:13 +0500

    fixed error in RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,29 +1,40 @@
 import rss from '@astrojs/rss';
-import { NUMBER_OF_POSTS_PER_PAGE } from '@/server-constants'
-import {
-  getPosts,
-  getRankedPosts,
-  getAllTags,
-} from '@/lib/notion/client.ts'
+// import { NUMBER_OF_POSTS_PER_PAGE } from '@/server-constants'
+// import {
+//   getPosts
+// } from '@/lib/notion/client.ts'
 
-const [posts, rankedPosts, tags ] = await Promise.all([
-  getPosts(NUMBER_OF_POSTS_PER_PAGE),
-  getRankedPosts(),
-  getAllTags(),
-])
+// const [posts ] = await Promise.all([
+//   getPosts(NUMBER_OF_POSTS_PER_PAGE),
+// ])
+
 export function GET(context) {
 
   return rss({
     // `<title>` field in output xml
-    title: 'Buzz’s Blog',
+    title: 'Mohib Blog',
     // `<description>` field in output xml
-    description: 'A humble Astronaut’s guide to the stars',
+    description: 'All the latest blogs on Tech and Software development',
     // Pull in your project "site" from the endpoint context
     // https://docs.astro.build/en/reference/api-reference/#contextsite
     site: context.site,
     // Array of `<item>`s in output xml
     // See "Generating items" section for examples using content collections and glob imports
-    posts, rankedPosts, tags,
+    // items : posts.map(post => ({
+    //     title : post.title,
+    //     link : `https://mohib.pages.dev/blog/${post.Slug}`,
+    //     pubDate : post.date,
+    //     content: post.excerpt,
+    //     categories: post.Tags.map(tag => tag.name),
+    //     author: "Muhabbat Ali"
+    //     // date: post.date,
+    //     // excerpt: post.Excerpt,
+    //     // tags : post.Tags.map(tag => ({
+    //     //     name: tag.name
+    //     // }))
+    // })),
+    items: [],
+
     // (optional) inject custom xml
     customData: `<language>en-us</language>`,
   });
